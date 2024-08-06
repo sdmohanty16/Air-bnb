346e55eb11df75592a3338cdb09f3601c250f8b3
LAPTOPS24 <sdmohanty16@gmail.com>
2024-08-06 12:49:34 +0530

    Airbnb JWT

---
+++ src/main/java/com/airbnb/service/JWTService.java
@@ -0,0 +1,30 @@
+package com.airbnb.service;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class JWTService {
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.expiryDuration}")
+    private int expiryTime;
+
+    private Algorithm algorithm;
+
+
+
+    @PostConstruct
+    public void postConstruct() throws UnsupportedEncodingException {
+        algorithm = Algorithm.HMAC256(algorithmKey);
+    }
+}
