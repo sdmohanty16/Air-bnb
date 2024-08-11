24f973194166756e509d05d4b29e92b360311b63
LAPTOPS24 <sdmohanty16@gmail.com>
2024-08-11 10:59:07 +0530

    JWT configure case3

--- src/main/java/com/airbnb/config/SecurityConfig.java
+++ src/main/java/com/airbnb/config/SecurityConfig.java
@@ -4,9 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private JWTFilter jwtFilter;
+
+    public SecurityConfig(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -16,10 +23,16 @@ public class SecurityConfig {
         //h(cd)2
         http.csrf().disable().cors().disable();
 
-        //harp
+        //haap
+        //http.authorizeHttpRequests().anyRequest().permitAll();
+
+        http.addFilterBefore(jwtFilter, AuthorizationFilter.class);
+        //harpaa
         http.authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/addUser")
-                .permitAll().anyRequest().authenticated();
+                .requestMatchers("/api/v1/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
         return http.build();
 
     }
